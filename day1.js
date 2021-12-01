3bdc217a15b247a61307c89f9bd2bd3ae9d82d1a
Shubham <shubham88singh@gmail.com>
2021-12-02 03:23:23 +0530

    Adding the solution for the second problem

--- day1.js
+++ day1.js
@@ -17,4 +17,26 @@ const getDepthMeasurement = (arr) => {
   return count;
 };
 
-console.log(getDepthMeasurement(values));
+const getThreeMeasurementSingleWindow = (arr) => {
+  let prevSum = 0;
+  let currentSum = 0,
+    count = 0;
+  result = [];
+  let windowIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    prevSum += Number(arr[i]);
+    currentSum = prevSum;
+    if (i >= 2) {
+      result.push(prevSum);
+      prevSum -= Number(arr[windowIndex]);
+      windowIndex += 1;
+    }
+  }
+  for (let i = 1; i < result.length; i += 1) {
+    if (result[i] - result[i - 1] > 0) count += 1;
+  }
+  return count;
+};
+// console.log(getDepthMeasurement(values));
+
+console.log(getThreeMeasurementSingleWindow(values));
