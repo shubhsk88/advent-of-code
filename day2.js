51cf72e9b78ebfda456ea0a0d8fc6332b9ae1879
Shubham <shubham88singh@gmail.com>
2021-12-02 13:58:34 +0530

    Adding the part 2 of advent code

--- day2.js
+++ day2.js
@@ -18,4 +18,26 @@ const getLocation = (arr) => {
   return xLocation * yLocation;
 };
 
-console.log(getLocation(values));
+// console.log(getLocation(values));
+
+const getLocationPart = (arr) => {
+  let xLocation = 0;
+  let yLocation = 0;
+  let depth = 0;
+  for (const item of arr) {
+    const [position, v] = item.split(' ');
+    const value = Number(v);
+    if (position === 'down') {
+      yLocation += value;
+    }
+    if (position === 'up') yLocation -= value;
+    if (position === 'forward') {
+      depth += yLocation * value;
+
+      xLocation += value;
+    }
+  }
+  return xLocation * depth;
+};
+
+console.log(getLocationPart(values));
