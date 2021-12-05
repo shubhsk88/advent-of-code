80d3f4b3d519106335dbfc36e97e67d80e48236b
Shubham <shubham88singh@gmail.com>
2021-12-05 21:33:55 +0530

    Adding the part one solution of day 5

---
+++ day5.js
@@ -0,0 +1,50 @@
+const { readDataFromFile } = require('./utils');
+
+const values = readDataFromFile('./day-5.txt');
+
+const calculatePoints = (arr) => {
+  const hash = {};
+  arr.pop();
+  let count = 0;
+  for (let item of arr) {
+    const str = item.replace(/\s+/gi, '').split('->');
+    console.log(str);
+    const firstPoint = str[0].split(',').map((item) => Number(item));
+    const secondPoint = str[1].split(',').map((item) => Number(item));
+
+    const startX =
+      firstPoint[0] > secondPoint[0] ? secondPoint[0] : firstPoint[0];
+    const endX =
+      firstPoint[0] > secondPoint[0] ? firstPoint[0] : secondPoint[0];
+
+    const startY =
+      firstPoint[1] > secondPoint[1] ? secondPoint[1] : firstPoint[1];
+    const endY =
+      firstPoint[1] > secondPoint[1] ? firstPoint[1] : secondPoint[1];
+    if (startX === endX) {
+      for (let i = startY; i <= endY; i += 1) {
+        const coord = `${startX},${i}`;
+        if (!hash[coord]) {
+          hash[coord] = 0;
+        }
+        hash[coord] += 1;
+      }
+    } else if (startY === endY) {
+      for (let i = startX; i <= endX; i += 1) {
+        const coord = `${i},${endY}`;
+        if (!hash[coord]) {
+          hash[coord] = 0;
+        }
+        hash[coord] += 1;
+      }
+    }
+  }
+  for (const item in hash) {
+    if (hash[item] >= 2) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+console.log(calculatePoints(values));
